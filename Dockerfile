fbf201688c19f3804bd840e2dd15b09bfce73c91
mikel <mikelduke@mdp3.net>
2017-10-24 21:14:41 -0500

    fix paths to use ~ instead of /home

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,12 @@ from jekyll/builder
 RUN apk --no-cache add git
 RUN gem install bundler
 
-RUN chown -R jekyll:jekyll /usr/local/* && \
+RUN chown -R jekyll:root /usr/local/* && \
     chmod -R 777 /usr/local/*
 RUN chmod u+s /sbin/su-exec
 
 ADD build.sh /home/jekyll
-RUN chown jekyll:jekyll /home/jekyll/build.sh && \
+RUN chown jekyll:root /home/jekyll/build.sh && \
     chmod -R 777 /home/jekyll/build.sh
 
 USER jekyll
--- build.sh
+++ build.sh
@@ -11,15 +11,16 @@ echo "$KEY" > ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 
 echo 'Clone Jekyll project repo'
+mkdir ~/jekyll
 git clone $GIT_REPO ~/jekyll
 
 echo 'jekyll build'
-cd /home/jekyll/jekyll
+cd ~/jekyll
 #gem install bundler
 bundle install
 jekyll build -s ~/jekyll -d ~/jekyll/_site
 
 echo 'scp'
-scp -rv -o "BatchMode yes" ~/jekyll/_site/* $SCP_DEST
+scp -r -o "BatchMode yes" ~/jekyll/_site/* $SCP_DEST
 
 echo 'done'
