4725a5ac3cfcc9b1b442e529ba41c98378da7d0d
mikel <mikelduke@mdp3.net>
2017-10-29 14:15:32 -0500

    use bundle exec for build to run on heroku

--- build.sh
+++ build.sh
@@ -11,16 +11,16 @@ echo "$KEY" > ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 
 echo 'Clone Jekyll project repo'
-mkdir ~/jekyll
-git clone $GIT_REPO ~/jekyll
+mkdir /jekyll
+git clone $GIT_REPO /jekyll
 
 echo 'jekyll build'
-cd ~/jekyll
+cd /jekyll
 #gem install bundler
 bundle install
-jekyll build -s ~/jekyll -d ~/jekyll/_site
+bundle exec jekyll build -s /jekyll -d /jekyll/_site
 
 echo 'scp'
-scp -r -o "BatchMode yes" ~/jekyll/_site/* $SCP_DEST
+scp -r -o "BatchMode yes" /jekyll/_site/* $SCP_DEST
 
 echo 'done'
